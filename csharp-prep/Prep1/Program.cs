caf190152d4c9c29ca9ffce0012cbef61ffe6a04
Pablopy10 <pablopy10@hotmail.com>
2024-09-21 23:30:11 +0100

    commit

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        string first = GetInput("What is your first name? ");
+        string last = GetInput("What is your last name? ");
+
+        PrintFullName(first, last);
+    }
+
+    static string GetInput(string prompt)
+    {
+        Console.Write(prompt);
+        return Console.ReadLine();
+    }
+
+    static void PrintFullName(string first, string last)
+    {
+        Console.WriteLine($"Your name is {last}, {first} {last}.");
     }
 }
